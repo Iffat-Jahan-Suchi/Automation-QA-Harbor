619c3cd7c333eee5bae2114544b50adaded888f2
Iffat Jahan <iffatjahan046@gmail.com>
2026-07-18 11:55:30 +0600

    convert only coin to cent

--- Exam/Question4.py
+++ Exam/Question4.py
@@ -1,7 +1,7 @@
 amount = float(input("Enter any number: "))
 amount = round(amount * 100)
 notes = [100, 50, 20, 10, 5, 2]
-coins = [100, 50, 25, 10, 5, 1]
+coins = [1, 0.50, 0.25, 0.10, 0.05, 0.01 ]
 print("NOTAS:")
 for note in notes:
     value = note * 100
@@ -11,18 +11,19 @@ for note in notes:
 print("MOEDAS:")
 
 for coin in coins:
-    count = amount // coin
-    if coin == 100:
+    val=coin*100
+    count = amount // val
+    if val == 100:
         print(f"{count} moeda(s) de R$ 1.00")
-    elif coin == 50:
+    elif val == 50:
         print(f"{count} moeda(s) de R$ 0.50")
-    elif coin == 25:
+    elif val == 25:
         print(f"{count} moeda(s) de R$ 0.25")
-    elif coin == 10:
+    elif val == 10:
         print(f"{count} moeda(s) de R$ 0.10")
-    elif coin == 5:
+    elif val == 5:
         print(f"{count} moeda(s) de R$ 0.05")
     else:
         print(f"{count} moeda(s) de R$ 0.01")
 
-    amount %= coin
+    amount%=val
